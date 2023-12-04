fddac092d353155c0eb2fef2ff731d0270a7cc4e
Raj Bedi <raj.j.bedi@gmail.com>
2023-12-04 10:31:19 -0500

    finished tutorial for farm collision shooter

--- Prototype2/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float speed = 20.0f;
     public float xRange = 10;
     public GameObject projectilePrefab;
 
@@ -20,11 +20,11 @@ public class PlayerController : MonoBehaviour
     {
         if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if(transform.position.x > xRange)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
